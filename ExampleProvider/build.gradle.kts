843505097ac1428c56e10b7b0aeb83fb8c8982fb
lilylagrange00 <100016480+lilylagrange00@users.noreply.github.com>
2025-11-24 19:49:07 -0600

    Update build.gradle.kts with new properties and values

--- ExampleProvider/build.gradle.kts
+++ ExampleProvider/build.gradle.kts
@@ -1,38 +1,28 @@
-dependencies {
-    implementation("com.google.android.material:material:1.12.0")
-    implementation("androidx.recyclerview:recyclerview:1.3.2")
-}
-
-// Use an integer for version numbers
+// use an integer for version numbers
 version = 1
 
-cloudstream {
-    // All of these properties are optional, you can safely remove any of them.
 
-    description = "Lorem ipsum"
-    authors = listOf("Cloudburst", "Luna712")
+cloudstream {
+    language = "en"
+    // All of these properties are optional, you can safely remove them
+
+    description = "Animes (SUB/DUB)"
+    authors = listOf("Lorem,Lorem Ipsum,")
 
     /**
-    * Status int as one of the following:
-    * 0: Down
-    * 1: Ok
-    * 2: Slow
-    * 3: Beta-only
-    **/
-    status = 1 // Will be 3 if unspecified
+     * Status int as the following:
+     * 0: Down
+     * 1: Ok
+     * 2: Slow
+     * 3: Beta only
+     * */
+    status = 1 // will be 3 if unspecified
+    tvTypes = listOf(
+        "AnimeMovie",
+        "Anime",
+        "OVA",
+    )
+    iconUrl = "https://www.google.com/s2/favicons?domain=animepahe.ru/&sz=%size%"
 
-    tvTypes = listOf("Movie")
-
-    requiresResources = true
-    language = "en"
-
-    // Random CC logo I found
-    iconUrl = "https://upload.wikimedia.org/wikipedia/commons/2/2f/Korduene_Logo.png"
-}
-
-android {
-    buildFeatures {
-        buildConfig = true
-        viewBinding = true
-    }
+    isCrossPlatform = true
 }
